8fb3cb56ef026d9fdfc7c415f230b97ff2540f34
Slavena Slavova <106436147+SlavenaS@users.noreply.github.com>
2024-11-27 11:30:00 +0200

    data base

--- upr19/upr19/View/Admin.cs
+++ upr19/upr19/View/Admin.cs
@@ -16,5 +16,16 @@ namespace upr19.View
         {
             InitializeComponent();
         }
+
+        private void Admin_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database11DataSet.Admin' table. You can move, or remove it, as needed.
+            this.adminTableAdapter.Fill(this.database11DataSet.Admin);
+            // TODO: This line of code loads data into the 'database11DataSet.Gallery' table. You can move, or remove it, as needed.
+        //    this.galleryTableAdapter.Fill(this.database11DataSet.Gallery);
+            // TODO: This line of code loads data into the 'database11DataSet.Admin' table. You can move, or remove it, as needed.
+          //  this.adminTableAdapter.Fill(this.database11DataSet.Admin);
+
+        }
     }
 }
